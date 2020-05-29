5bb759704fc150ed689b9adb092bbf8e3d728a11
truehello <trudelmarc@gmail.com>
2020-05-29 16:55:07 -0700

    update API 2

--- functions/weather/weather.js
+++ functions/weather/weather.js
@@ -7,7 +7,7 @@ exports.handler = async (event, context) => {
     const lng = event.queryStringParameters.lng || 139;
     const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric&appid=${WEATHER_API_KEY}`;
     //const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lng}&units=metric`;
-
+    console.log(url)
 
   try {
     const response = await fetch(url, {

eab7a28b5bd7e783e158b444e43cecc12de68433
truehello <trudelmarc@gmail.com>
2020-06-04 12:31:03 -0700

    added ip location lookup

--- functions/weather/weather.js
+++ functions/weather/weather.js
@@ -1,18 +1,15 @@
-const fetch = require("node-fetch");
+const fetch = require("node-fetch")
 
-const WEATHER_API_KEY = process.env.WEATHER_API_KEY;
+const WEATHER_API_KEY = process.env.WEATHER_API_KEY
 
-exports.handler = async (event, context) => {
-    const lat = event.queryStringParameters.lat || 51.51;
-    const lon = event.queryStringParameters.lng || -0.13;
+// get the geolocation from the IP in the header
+let getLocationFromIP = async userIP => {
+  if (userIP === "::1") {
+    userIP = "24.69.174.90"
+  }
+  console.log("ip in func =" + userIP)
 
-    //const userIP = event.requestContext.identity.sourceIp
-    //const userIP = context.identity.sourceIp
-    const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`;
-    //console.log("context ="+JSON.stringify(context))
-    //console.log("x-forwarded-for ="+ event.headers['x-forwarded-for'])
-    console.log("IP ="+JSON.stringify( event.headers['client-ip']))
-    //event.headers['x-requested-with']
+  const url = `http://ip-api.com/json/${userIP}`
 
   try {
     const response = await fetch(url, {
@@ -20,26 +17,69 @@ exports.handler = async (event, context) => {
       headers: {
         Accept: "*/*",
         "Content-Type": "application/json",
-        "X-Api-Key": WEATHER_API_KEY,
-        'Access-Control-Allow-Origin' : '*',
-      }
-    });
-    const data = await response.json();
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+    const data = await response.json()
 
     if (!response.ok) {
       // NOT res.status >= 200 && res.status < 300
-      return { statusCode: data.status, body: data.detail };
+      return { statusCode: data.status, body: data.detail }
+    }
+    return {
+      data,
+    }
+  } catch (err) {
+    console.log(err)
+  }
+}
+
+// get the weather for the location
+let getWeatherForLocation = async location => {
+  //console.log(location.data)
+  const lat = location.data.lat || 51.51
+  const lon = location.data.lon || -0.13
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric`
+  try {
+    const response = await fetch(url, {
+      method: "GET",
+      headers: {
+        Accept: "*/*",
+        "Content-Type": "application/json",
+        "X-Api-Key": WEATHER_API_KEY,
+        "Access-Control-Allow-Origin": "*",
+      },
+    })
+    const data = await response.json()
+
+    if (!response.ok) {
+      // NOT res.status >= 200 && res.status < 300
+      return { statusCode: data.status, body: data.detail }
     }
     return {
       statusCode: 200,
       body: JSON.stringify({ msg: "success", detail: data }),
-    };
+    }
   } catch (err) {
-    console.log(err); // output to netlify function log
+    console.log(err) // output to netlify function log
     return {
       statusCode: 500,
       body: JSON.stringify({ msg: err.message }),
-    };
+    }
   }
-};
+}
 
+exports.handler = async (event, context) => {
+  const userIP = event.headers["client-ip"] || "24.69.174.90"
+
+  try {
+    const location = await getLocationFromIP(userIP)
+
+    const weather = await getWeatherForLocation(location)
+
+    return weather
+  } catch (err) {
+    console.log(err)
+  }
+}
--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,15 +1,1 @@
 import "./src/css/tailwind.css"
-
-//const React = require("react");
-//import wrapRootElement from "./wrap-root-element"
-//const wrapRootElement = require("./wrap-root-element");
-
-//exports.wrapRootElement = wrapRootElement;
-
-//exports.wrapRootElement = ({ element }) => {
- //   return (
-     
- //       {wrapRootElement({ element })}
-     
- //   );
-//  };
